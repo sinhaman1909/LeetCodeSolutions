1e3caf34b2c3497c2e880b65e79522a60b77a4fa
sinhaman1909 <50315327+sinhaman1909@users.noreply.github.com>
2022-01-08 12:29:14 +0530

    Time: 195 ms (26.94%), Space: 15.5 MB (83.42%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -2,10 +2,11 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         freqCount = {}
         for element in nums:
-            if element not in freqCount.keys():
-                freqCount[element] = 1
+            if element in freqCount.keys():
+                freqCount[element] = freqCount[element] + 1
             else:
-                freqCount.update({ element : freqCount[element] + 1})
-                if freqCount[element] > len(nums) // 2:
-                    return element
-        return element
+                freqCount[element] = 1
+        
+        for key in freqCount.keys():
+            if freqCount[key] > len(nums) // 2:
+                return key
