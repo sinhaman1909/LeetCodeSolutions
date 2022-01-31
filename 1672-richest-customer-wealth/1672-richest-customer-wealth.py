f5cdc5b1fea0059acd1a00534d0e924bdc292548
sinhaman1909 <50315327+sinhaman1909@users.noreply.github.com>
2022-01-31 18:50:06 +0530

    Time: 86 ms (25.14%), Space: 13.9 MB (96.85%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -2,7 +2,8 @@ class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         _max = -float(inf)
         for customer_wealth in accounts:
-            _max = max(_max, sum(customer_wealth))
+            if sum(customer_wealth) > _max:
+                _max = sum(customer_wealth)
             
         return _max
         
