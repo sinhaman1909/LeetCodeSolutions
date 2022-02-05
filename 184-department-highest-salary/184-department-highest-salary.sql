b6a31c6f5080fa4dcb6892c53308b53b3e30621c
sinhaman1909 <50315327+sinhaman1909@users.noreply.github.com>
2022-02-06 03:01:20 +0530

    Time: 853 ms (15.59%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -2,7 +2,7 @@
 select d.name as Department, e.name as Employee,
 e.salary as Salary
 from Employee e
-join Department d
+left join Department d
 on e.departmentId = d.id
 where 
 (e.departmentId, e.salary) in
