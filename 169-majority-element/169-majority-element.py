31ce8ec2f6c8f1796a951c356082fb3de4ab041b
sinhaman1909 <50315327+sinhaman1909@users.noreply.github.com>
2022-02-21 23:47:10 +0530

    Time: 445 ms (5.09%), Space: 15.5 MB (81.81%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,12 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freqCount = {}
-        for element in nums:
-            if element in freqCount.keys():
-                freqCount[element] = freqCount[element] + 1
-            else:
-                freqCount[element] = 1
+        hashmap = {}
         
-        for key in freqCount.keys():
-            if freqCount[key] > len(nums) // 2:
-                return key
+        for n in nums:
+            if n in hashmap:
+                hashmap[n] = hashmap[n] + 1
+            else:
+                hashmap[n] = 1
+                
+            if hashmap[n] > len(nums) // 2:
+                return n 
+        
